2e38d957da31b475b2e5f64f1c12a9f9d55c5d2c
Shaun Davis <sdavis922@gmail.com>
2017-04-24 12:39:46 -0500

    Added search page.

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<div class="input-group">
+	<form action="<?php esc_url( home_url( '/' ) ); ?>" role="search" method="get">
+		<span class="input-group-btn">
+		<input type="search" name="s" class="form-control" placeholder="Search for...">
+			<button class="btn btn-default" type="submit">Go</button>
+		</span>
+	</form>
+</div>
