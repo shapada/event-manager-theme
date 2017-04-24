098d44f9c4ec1a4317edd1d9a588622bfbe348b7
Shaun Davis <sdavis922@gmail.com>
2017-04-24 13:05:26 -0500

    Fixed refinement error

--- functions.php
+++ functions.php
@@ -11,3 +11,17 @@ function event_manager_menus() {
 	register_nav_menu('primary',__( 'Primary Menu' ));
 }
 add_action( 'init', 'event_manager_menus' );
+
+function event_manager_logo() { ?>
+	<style type="text/css">
+		body.login div#login h1 a {
+			background-image: none!important;
+			height:65px;
+			width:320px;
+			background-size: 320px 65px;
+			background-repeat: no-repeat;
+			padding-bottom: 30px;
+		}
+	</style>
+<?php }
+add_action( 'login_enqueue_scripts', 'event_manager_logo' );
