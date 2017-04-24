1c79ed3681b51069190ce3bb6fa0616fcde40e31
Shaun Davis <sdavis922@gmail.com>
2017-04-24 13:33:49 -0500

    The single entertainer page is now dynamically rendering the entertainer info.

--- single-entertainer.php
+++ single-entertainer.php
@@ -8,13 +8,35 @@ get_header(); ?>
 			<div class="x_panel">
 				<?php get_template_part( 'template-parts/panel-header' ); ?>
 
-				<div class="x_content">
+                <div class="x_content">
 
-				</div>
-			</div>
+                    <div class="col-md-3 col-sm-3 col-xs-12 profile_left">
+
+                        <img class="img-responsive" src="<?php echo get_field( 'image' )['url']; ?>">
+                        
+                        <h3><?php the_title(); ?></h3>
+
+	                    <?php $entertainer_type = get_the_terms( get_the_ID(), 'entertainer_type' ); ?>
+                        <ul class="list-unstyled user_data">
+                            <li>Type: <?php echo "{$entertainer_type[0]->name}"; ?></li>
+                        </ul>
+
+                    </div>
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        <div class="profile_title">
+                            <div class="col-md-6">
+                                <h2>About the Entertainer</h2>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <p><?php echo get_field( 'description' ); ?></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 		</div>
 		<div class="clearfix"></div>
 	</div>
-
 
 <?php get_footer(); ?>
--- template-parts/panel-header.php
+++ template-parts/panel-header.php
@@ -1,12 +1,16 @@
 <div class="x_title">
     <?php
         $title = '';
-        if ( is_front_page() || is_singular()  ) {
+        if ( is_front_page() ) {
             $title = the_title();
         } elseif ( is_page( 'entertainers' ) || is_page( 'venues' ) ) {
             $title = the_title( 'All' );
         } elseif ( is_search() ) {
             $title = 'Search Results';
+        } elseif ( is_singular( 'entertainer' ) ) {
+            $title = the_title( 'Entertainer Profile: ' );
+        } elseif ( is_singular( 'venue' ) ) {
+	        $title = the_title( 'Venue Profile: ' );
         }
     ?>
     <h2><?php echo $title; ?></h2>
